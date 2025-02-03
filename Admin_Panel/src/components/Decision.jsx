e4bf7c7f121938264116548fc549df17b0ba6500
Hadrey047 <hadreytechsolutions@gmail.com>
2025-02-03 17:02:26 +0100

    Third commit

--- Admin_Panel/src/components/Decision.jsx
+++ Admin_Panel/src/components/Decision.jsx
@@ -3,8 +3,20 @@ import '../styles/Decision.css'
 
 const Decision = () => {
     return (
-        <div>
-            jfjfj
+        <div className='decision'>
+            <a href='#' className='item'>
+                    Overview
+            </a>
+            <a href='#' className='item'>
+                    Branch
+            </a>
+            <a href='#' className='item'>
+                    KYC Document
+            </a>
+            <a href='#' className='item'>
+                    Activity Log
+            </a>
+
         </div>
     );
 }
--- Admin_Panel/src/components/User.jsx
+++ Admin_Panel/src/components/User.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import peppermint from '../Images/peppermint.jpeg'
+import '../styles/User.css'
 
 const User = () => {
     return (
-        <div>
-            User Profile 
+        <div className='user-profile'>
+            <div className='user-pic'>
+                <img src={peppermint} />
+            <div className='user-name'>
+                <h3>Abdullah Nasir</h3></div>
+            </div>
         </div>
     );
 }
